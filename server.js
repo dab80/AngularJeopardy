2898d4d307d1a23354607456868b6aacd26109f1
Dave Bell <david.bell@libertymutual.com>
2017-07-13 08:29:51 -0400

    added SSL to server.js

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+app.use(forceSSL());
